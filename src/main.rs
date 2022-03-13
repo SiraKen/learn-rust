e0df4e5f37b6879c71fcdd33d5f89978bdc9a836
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-03-13 11:21:44 +0900

    feat: add some funcs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,35 @@
 fn main() {
     println!("Hello, world!");
-    for n in 1..10 {
-        println!("{n}")
-    }
+    println!("{}", add(1, 2)); // -> 3
+    println!("{}", sub(1, 2)); // -> -1
+    println!("{}", mul(1, 2)); // -> 2
+    println!("{}", div(1, 2)); // -> 0.5
+    println!("{}", avg(1, 2)); // -> 
+}
+
+fn add(x: i32, y: i32) -> i32 {
+    return x + y;
+}
+
+fn sub(x: i32, y: i32) -> i32 {
+    return x - y;
+}
+
+fn mul(x: i32, y: i32) -> i32 {
+    return x * y;
+}
+
+fn div(x: i32, y: i32) -> f32 {
+    return (x / y) as f32;
+}
+
+fn avg(x: i32, y: i32) -> f32 {
+    return ((x + y) / 2) as f32;
+}
+
+#[test]
+fn it_works() {
+    // FIXME: test failed
+    assert_eq!(div(1, 2), 0.5);
+    assert_eq!(div(1, 2), 0.5);
 }
